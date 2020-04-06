c80777f662b6ff5cd4af693b2be6469ad1782817
Rizwan <mdrizwan197@gmail.com>
2020-04-06 13:45:50 +0200

    Plotfile added to compare two flowsheets

--- plotSSflowsheets.m
+++ plotSSflowsheets.m
@@ -3,10 +3,14 @@ clear
 close all
 
 load Data_CoupledElectrolyzer
+load Data_DecoupledElectrolyzers
 
 set(0, 'DefaultAxesFontSize', 12,  'DefaultLineLineWidth', 1.5)
 fig1 = figure('NumberTitle', 'off', 'Name', 'Electrolyzer current');
 hold on
-plot(row1(:,2),row1(:,end))
+plot(row1(:,2),row1(:,end))%plot for lower Pnet needs to be fixed
+hold on
+plot(row(:,2),row(:,end))
 grid on
+legend('Flowsheet 1','Flowsheet 2')
 saveas(fig1,'FlowsheetResult','epsc');
